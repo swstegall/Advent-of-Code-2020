23e5ea457e0563a0d2901ee2f812405df0238f2c
Sam Stegall <stegall@medicine.tamhsc.edu>
2020-12-03 13:11:41 -0600

    Solved problem 3.

--- problem_3/src/main.rs
+++ problem_3/src/main.rs
@@ -14,12 +14,25 @@ fn parse_input(file_name: &str) -> Vec<String> {
     v
 }
 
-fn count_trees(lines: &Vec<String>) -> i32 {
-    0
+fn count_trees(lines: &Vec<String>, x_jump_size: usize, y_jump_size: usize) -> u32 {
+    let width = lines[0].len();
+    lines.iter()
+        .enumerate()
+        .filter(|(i, line)| {
+            let mut chars = line.chars();
+            (i % y_jump_size == 0) && (chars.nth(x_jump_size * i / y_jump_size % width).unwrap() == '#')
+        })
+        .count() as u32
 }
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let lines: Vec<String> = parse_input("input.txt");
-    let trees: i32 = count_trees(&lines);
-    println!("{}", &trees);
+    let trees: u32 = count_trees(&lines, 1, 1);
+    let trees1: u32 = count_trees(&lines, 3, 1);
+    let trees2: u32 = count_trees(&lines, 5, 1);
+    let trees3: u32 = count_trees(&lines, 7, 1);
+    let trees4: u32 = count_trees(&lines, 1, 2);
+    let result: u32 = &trees * &trees1 * &trees2 * &trees3 * &trees4;
+    println!("{}", result);
+    Ok(())
 }
